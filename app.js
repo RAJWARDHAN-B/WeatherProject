1be4641d294ea8b49f3c92b8b8967cb47d90f747
RAJWARDHAN-B <147420707+RAJWARDHAN-B@users.noreply.github.com>
2024-08-25 22:07:17 +0530

    A simple web application using api

---
+++ app.js
@@ -0,0 +1,16 @@
+// jshint esversion: 6
+
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/",function(req,res){
+    
+
+    res.send("Server is up and running");
+})
+
+app.listen(3000, function(){
+    console.log("Server running on port 3000...");
+});
