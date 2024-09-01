d619f853216e076f052d6697dabe77f20b1f0be7
RAJWARDHAN-B <147420707+RAJWARDHAN-B@users.noreply.github.com>
2024-09-01 18:06:28 +0530

    created form and used body-parser to get user input and then display weather of that city.

--- app.js
+++ app.js
@@ -2,12 +2,31 @@
 
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/",function(req,res){
+
+    res.sendFile(__dirname + "/index.html");
+        
     
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Pune&appId=0582caf2ba797f537d8b341735ab23ec&units=metric";
+    //res.send("Server is up and running");
+});
+
+app.post("/", function(req,res){
+
+    //console.log(req.body.cityName);
+    //console.log("Post request received");
+
+    const query = req.body.cityName;
+    const apiKey = "0582caf2ba797f537d8b341735ab23ec";
+    const unit = "metric";
+    
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appId="+ apiKey +"&units=" + unit;
+    
     https.get(url,function(response){
         console.log(response.statusCode);
         response.on("data",function(data){
@@ -20,17 +39,18 @@ app.get("/",function(req,res){
             const imageURL = "https://openweathermap.org/img/wn/" + icon +"@2x.png";
 
             res.write("<P>The current weather is " + weatherdescription + "</P>");
-            res.write("<h1>The temperature in Pune is: "+ temp + " degrees celsius.</h1>");
+            res.write("<h1>The temperature in " + query + " is: "+ temp + " degrees celsius.</h1>");
             res.write("<img src="+imageURL+" >")
             res.send();
            
             // WE CAN ONLY HAVE ONE RES.SEND
             console.log(weatherdescription);
         });
-        
+
     });
-    //res.send("Server is up and running");
 });
+
+
 
 app.listen(3000, function(){
     console.log("Server running on port 3000...");
