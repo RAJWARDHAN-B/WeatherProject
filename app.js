332c01397a5b9b81edf9718f20de1de8c928e643
RAJWARDHAN-B <147420707+RAJWARDHAN-B@users.noreply.github.com>
2024-09-01 17:31:32 +0530

    displaying the temperature on console

--- app.js
+++ app.js
@@ -10,10 +10,20 @@ app.get("/",function(req,res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q=Pune&appId=0582caf2ba797f537d8b341735ab23ec&units=metric";
     https.get(url,function(response){
         console.log(response.statusCode);
+        response.on("data",function(data){
+            const weatherData = JSON.parse(data);
+            console.log(weatherData);
+            const weatherdescription = weatherData.weather[0].description;
+            const temp = weatherData.main.temp;
+
+            res.send("The temperature in Pune is: "+ temp + " degrees celsius.");
+           
+            console.log(weatherdescription);
+        });
         
     });
-    res.send("Server is up and running");
-})
+    //res.send("Server is up and running");
+});
 
 app.listen(3000, function(){
     console.log("Server running on port 3000...");
